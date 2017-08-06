b81f5cf932c0b3645563da73d630f0f8d568d449
Jeorge Taflanidi <jeorge.morpheus@gmail.com>
2017-08-06 04:50:31 +0300

    macOS target introduced. Test target cleanup.

---
+++ Source/InputMask/InputMaskMac/InputMaskMac.h
@@ -0,0 +1,19 @@
+//
+//  InputMaskMac.h
+//  InputMaskMac
+//
+//  Created by Jeorge Taflanidi on 8/6/29 H.
+//  Copyright © 29 Heisei Egor Taflanidi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for InputMaskMac.
+FOUNDATION_EXPORT double InputMaskMacVersionNumber;
+
+//! Project version string for InputMaskMac.
+FOUNDATION_EXPORT const unsigned char InputMaskMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InputMaskMac/PublicHeader.h>
+
+
