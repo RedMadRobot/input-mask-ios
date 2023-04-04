02f1aeff902bb7bea5daff324090a8b055e97ffe
taflanidi <jeorge.morpheus@gmail.com>
2023-04-04 17:32:59 +0400

    Allow dangling decimal separator & dangling zeros

--- Source/InputMask/InputMask/Classes/View/NumberInputListener.swift
+++ Source/InputMask/InputMask/Classes/View/NumberInputListener.swift
@@ -25,17 +25,54 @@ open class NumberInputListener: MaskedTextInputListener {
             return ok
         }
         
-        let decimalNumber: String = number
+        var suffix = ""
+        if let lastChar = number.last, !CharacterSet.decimalDigits.isMember(character: lastChar) {
+            suffix = String(lastChar) // suffix is a dangling decimal separator
+        }
+        
+        var decimalNumber: String = number
             .replacingOccurrences(of: decimalSeparator, with: ".")
             .replacingOccurrences(of: currencyDecimalSeparator, with: ".")
+        
+        if decimalNumber.contains(".") {
+            let components = decimalNumber.components(separatedBy: ".")
+            let intPart = components.first ?? ""
+            var decPart = components.last ?? ""
+            
+            if decPart.count > formatter.maximumFractionDigits {
+                decPart = String(decPart[...decPart.startIndex(offsetBy: formatter.maximumFractionDigits)])
+            }
+            decimalNumber = "\(intPart).\(decPart)"
+        }
+        
+        if suffix.isEmpty && decimalNumber.contains(".") {
+            for char in decimalNumber.reversed {
+                if char != "0" && char != "." {
+                    break
+                }
+                suffix = String(char) + suffix
+            }
+        }
         
         guard let number = NumberFormatter().number(from: decimalNumber), let maskFormat = formatter.string(from: number)
         else {
             return super.pickMask(forText: text)
         }
         
-        primaryMaskFormat = "{\(maskFormat)}"
+        primaryMaskFormat = composeFormat(fromText: maskFormat + suffix)
         return super.pickMask(forText: text)
+    }
+    
+    open func composeFormat(fromText text: String) -> String {
+        var result = ""
+        text.forEach { character in
+            if CharacterSet.decimalDigits.isMember(character: character) {
+                result += "[0]"
+            } else {
+                result += "{\(character)}"
+            }
+        }
+        return result
     }
 }
 
