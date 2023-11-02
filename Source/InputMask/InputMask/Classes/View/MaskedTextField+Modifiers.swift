b3e1ad325ca895d7d8ac534d20b9c8f94de306e3
Vadym Brusko <vaadim.br@gmail.com>
2023-11-02 13:32:41 +0200

    Fix typo

--- Source/InputMask/InputMask/Classes/View/MaskedTextField+Modifiers.swift
+++ Source/InputMask/InputMask/Classes/View/MaskedTextField+Modifiers.swift
@@ -24,7 +24,7 @@ public extension MaskedTextField {
     /**
      ``UITextField/placeholderColor``
      */
-    func placehodelrColor(_ color: UIColor) -> Self {
+    func placehodlderColor(_ color: UIColor) -> Self {
         var s = self
         s.placeholderColor = color
         return s
